dab26dc410952096a4873f0e3c9aa27dea06e2b2
Yoda <YodaPY@github.com>
2021-03-28 15:05:44 +0200

    init

---
+++ builder/bot/items/__init__.py
@@ -0,0 +1,14 @@
+import yaml
+from typing import Dict, Any
+from builder.bot.paths import *
+
+class Item:
+    def __init__(self, name: str, /) -> None:
+        with open(ITEM_DEFINITION_PATH.format(name)) as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+
+        self.init_item(data)
+        
+    def init_item(self, data: Dict[str, Any], /) -> None:
+        for key, value in data.items():
+            setattr(self, key, value)
---
+++ builder/bot/paths.py
@@ -0,0 +1,7 @@
+__all__ = (
+    "ITEM_DEFINITION_PATH", "ITEM_IMAGE_PATH"
+)
+
+ITEM_PATH = "builder/bot/items"
+ITEM_DEFINITION_PATH = ITEM_PATH + "/definitions/{}"
+ITEM_IMAGE_PATH = ITEM_PATH + "/images/{}"
